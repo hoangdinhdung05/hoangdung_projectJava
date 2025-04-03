c0a3b36569375318968ef812746baf876a4f550d
hoangdinhdung05 <hoangdinhdung0205@gmail.com>
2025-04-03 21:39:09 +0700

    Dùng JWT lấy dữ liệu + response lỗi jwt

--- src/main/java/vn/hoangdung/projectJava/helpers/JwtAuthFilter.java
+++ src/main/java/vn/hoangdung/projectJava/helpers/JwtAuthFilter.java
@@ -1,23 +1,28 @@
 package vn.hoangdung.projectJava.helpers;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import vn.hoangdung.projectJava.modules.users.services.impl.CustomUserDetailsService;
-import vn.hoangdung.projectJava.modules.users.services.impl.UserService;
 import vn.hoangdung.projectJava.services.JwtService;
 
 @Component
@@ -26,7 +31,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     
     private final JwtService jwtService;
     private final CustomUserDetailsService customUserDetailsService;
-
+    private final ObjectMapper objectMapper;
     private static final Logger logger = LoggerFactory.getLogger(JwtAuthFilter.class);
 
     @Override
@@ -34,33 +39,135 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
+    
+        try {
 
+            final String authHeader = request.getHeader("Authorization");
+            final String jwt;
+            final String userId;
 
-        //Lấy token từ header
-        final String authHeader = request.getHeader("Authorization");
-        final String userId;
-        final String jwt;
+            //Check header token
+            if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+                // logger.info("Token không hợp lệ");
+                sendErrorResponse(
+                    response, 
+                    request, 
+                    HttpServletResponse.SC_INTERNAL_SERVER_ERROR, 
+                    "Xác thực không thành công", 
+                    "Không tìm thấy token"
+                );
+                return;
+            }
 
-        //check token có tồn tại không || null
-        if(authHeader == null || !authHeader.startsWith("Bearer")) {
+            //lấy token từ header
+            jwt = authHeader.substring(7);
+            //Check token 
+            if(!this.jwtService.isTokenFormatValid(jwt)) {
+                sendErrorResponse(
+                    response, 
+                    request,
+                    HttpServletResponse.SC_UNAUTHORIZED,
+                    "Xác thực không thành công",
+                    "Token không đúng định dạng"
+                );
+                return;
+            }
+            if(!this.jwtService.isTokenExpired(jwt)) {
+                sendErrorResponse(
+                    response,
+                    request,
+                    HttpServletResponse.SC_UNAUTHORIZED,
+                    "Xác thực không thành công",
+                    "Token đã hết hạn"
+                );
+                return;
+            }
+            if(!jwtService.isSignatureValid(jwt)){
+                sendErrorResponse(response, 
+                    request, 
+                    HttpServletResponse.SC_UNAUTHORIZED, 
+                    "Xác thực không thành công", 
+                    "Chữ ký không hợp lệ."
+                );
+                return;
+            }
 
-            logger.error("Token miss");
+            if(!jwtService.isIssuerToken(jwt)){
+                sendErrorResponse(response, 
+                    request, 
+                    HttpServletResponse.SC_UNAUTHORIZED, 
+                    "Xác thực không thành công", 
+                    "Nguồn Token không hợp lệ."
+                );
+                return;
+            }
+            
+            //Lấy userId từ token
+            userId = this.jwtService.getUserIdFromJwt(jwt);
+            if(userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = this.customUserDetailsService.loadUserByUsername(userId);
+                //get Username from token
+                String emailFromToken = this.jwtService.getEmailFromJwt(jwt);
+                //Check username 
+                if(!emailFromToken.equals(userDetails.getUsername())) {
+                    sendErrorResponse(
+                        response, 
+                        request, 
+                        HttpServletResponse.SC_UNAUTHORIZED, 
+                        "Xác thực không thành công", 
+                        "Tài khoản không hợp lệ."
+                    );
+                    return;
+                }
+                //username hợp lệ
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    userDetails, 
+                    null, 
+                    userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                    new WebAuthenticationDetailsSource()
+                        .buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+                logger.info("Xác thực thành công");
 
-            filterChain.doFilter(request, response);
-            return;
+            }
+
+            filterChain.doFilter(request, response); 
+
+        } catch (ServletException | IOException e) {
+            sendErrorResponse(response, 
+                request, 
+                HttpServletResponse.SC_INTERNAL_SERVER_ERROR, 
+                "Network Error!", 
+                e.getMessage()
+            );
         }
+        
+    }
 
-        jwt = authHeader.substring(7);
-        userId = this.jwtService.getUserIdFromJwt(jwt);
+    private void sendErrorResponse(
+        @NotNull HttpServletResponse response,
+        @NotNull HttpServletRequest request,
+        int statusCode,
+        String error,
+        String message) throws IOException {
 
-        if(userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.customUserDetailsService.loadUserByUsername(userId);
-            logger.info(userDetails.getUsername());
-        }
+        response.setStatus(statusCode);
+        response.setContentType("application/json;charset=UTF-8");
 
-        filterChain.doFilter(request, response);
+        Map<String, Object> errorResponse = new HashMap<>(); 
 
+        errorResponse.put("timestamp", System.currentTimeMillis());
+        errorResponse.put("status", statusCode);
+        errorResponse.put("error", error);
+        errorResponse.put("message", message);
+        errorResponse.put("path", request.getRequestURI());
 
+        String jsonResponse = objectMapper.writeValueAsString(errorResponse);
+
+        response.getWriter().write(jsonResponse);
     }
 
 }
--- src/main/java/vn/hoangdung/projectJava/services/JwtService.java
+++ src/main/java/vn/hoangdung/projectJava/services/JwtService.java
@@ -3,9 +3,7 @@ package vn.hoangdung.projectJava.services;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
-
 import org.springframework.stereotype.Service;
-
 import vn.hoangdung.projectJava.config.JwtConfig;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -15,6 +13,7 @@ import io.jsonwebtoken.security.Keys;
 @Service
 public class JwtService {
     
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(JwtService.class);
     private final JwtConfig jwtConfig;
     private final Key key;
 
@@ -31,6 +30,7 @@ public class JwtService {
                 .setSubject(String.valueOf(userId))
                 .claim("email", email)
                 .setIssuedAt(now)
+                .setIssuer(jwtConfig.getIssuer())
                 .setExpiration(expiryDate)
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
@@ -41,20 +41,21 @@ public class JwtService {
     //     return extractClaim(token, Claims::getSubject);
     // }
 
-    // private <T> T extractClaim(String token, java.util.function.Function<Claims, T> claimsResolver) {
-    //     final Claims claims = extractAllClaims(token);
-    //     return claimsResolver.apply(claims);
-    // }
+    private <T> T getClaimFromToken(String token, java.util.function.Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
 
     // //giải mã và lấy ra claims
-    // private Claims extractAllClaims(String token) {
-    //     return Jwts.parserBuilder()
-    //             .setSigningKey(key)
-    //             .build()
-    //             .parseClaimsJws(token)
-    //             .getBody();
-    // }
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 
+    //Lấy userId từ token
     public String getUserIdFromJwt(String token) {
         Claims claims = Jwts.parserBuilder()
                 .setSigningKey(key)
@@ -62,6 +63,113 @@ public class JwtService {
                 .parseClaimsJws(token)
                 .getBody();
         return claims.getSubject();
+    }
+
+    public String getEmailFromJwt(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.get("email", String.class);
+    }
+
+    //Kiểm tra token có hợp lệ không
+    
+    /*
+     * 1. Check token có đúng định dạng không
+     * 2. Check token có hết hạn không
+     * 3. Check secret có đúng không
+     * 4. Check userId có tồn tại không (có matches với userId trong DB không)
+     * 5. Check token có trong blacklist không
+     */
+
+    // public boolean isValidToken(String token, UserDetails userDetails) {
+    //     try {
+    //         //1. Check định dạng token
+    //         if(!isTokenFormatValid(token)) {
+    //             logger.info("Token không đúng định dạng");
+    //             return false;
+    //         }
+    //         //2. Check secret có đúng không
+    //         if(!isSignatureValid(token)) {
+    //             logger.info("Token không đúng secret");
+    //             return false;
+    //         }
+    //         //3. Check token có hết hạn không
+    //         if(!isTokenExpired(token)) {
+    //             logger.info("Token đã hết hạn");
+    //             return false;
+    //         }
+    //         //4. Check nguồn gốc
+    //         if(!isIssuerToken(token)) {
+    //             logger.info("Token không đúng nguồn gốc");
+    //             return false;
+    //         }
+    //         //5. Check username có tồn tại không
+    //         final String emailFromToken = getEmailFromJwt(token);
+    //         if(!emailFromToken.equals(userDetails.getUsername())) {
+    //             logger.info("UserToken không hợp lệ");
+    //             return false;
+    //         }
+    //         //6. Check blackListToken
+    //     } catch (Exception e) {
+    //         // TODO: handle exception
+    //         logger.info("Xác thực token thất bại" + e.getMessage());
+    //         return false;
+    //     }
+    //     return false;
+    // }
+
+    //Check định dạng
+    public boolean isTokenFormatValid(String token) {
+        try {
+            //Có 3 phần tử, mỗi phần tử cách nhau bởi dấu "."
+            //Cắt chuỗi thành 3 phần tử
+            String[] parts = token.split("\\.");
+            return parts.length == 3;
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            // logger.info("Token không hợp lệ" + e.getMessage());
+            return false;
+        }
+    }
+
+    //Check secret
+    public boolean isSignatureValid(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSecretKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            // TODO: handle exception
+            return false;
+        }
+    }
+
+    //Get secret
+    public Key getSecretKey() {
+        byte[] keyBytes = this.jwtConfig.getSecretKey().getBytes();
+        return Keys.hmacShaKeyFor(Base64.getEncoder().encode(keyBytes));
+    }
+
+    //Check expired
+    public boolean isTokenExpired(String token) {
+        try {
+            final Date expiration = getClaimFromToken(token, Claims::getExpiration);
+            return expiration.before(new Date());
+        } catch (Exception e) {
+            // TODO: handle exception
+            return false;
+        }
+    }
+
+    public boolean isIssuerToken(String token) {
+        String tokenIssuer = getClaimFromToken(token, Claims::getIssuer);
+        return this.jwtConfig.getIssuer().equals(tokenIssuer);
     }
 
 }
